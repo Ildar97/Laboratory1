d9bbcbc952ae6d1c89312a9ee1f0448492d1cddb
Ildar <tecktonik-ildar@mail.ru>
2016-12-19 10:15:07 +0500

    1 задание

--- qer.py
+++ qer.py
@@ -8,3 +8,21 @@ def MaxAndMinElement (a = []):
         if a[i] < min:
             min = a[i]
     return max,min
+
+def Fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return Fibonacci(n - 1) + Fibonacci(n - 2)
+
+
+def IsConsonantsLetters(word):
+    ConsonantsLetters = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x",
+                         "z", "б", "в", "г", "д", "ж", "з", "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ц", "ч",
+                         "ш", "щ"]
+    for i in range(len(word)):
+        for j in range(len(ConsonantsLetters)):
+            if word[i].lower() == ConsonantsLetters[j]:
+                return True
+    return False
